e56d154b9a49ca112bf0cfc214046ff15d3e0f26
jp.mandon <jp.mandon@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2011-02-14 14:08:15 +0000

    modified folder

---
+++ branches/pic32/C32/linux/tmp/define.h
@@ -0,0 +1,17 @@
+
+
+#define BIN 5
+#define BYTE 2
+#define DEC 1
+#define FALSE 0
+#define HEX 3
+#define HIGH 1
+#define INPUT 1
+#define LOW 0
+#define OCTAL 4
+#define OFF 0
+#define ON 1
+#define OUTPUT 0
+#define TRUE 1
+#include <digitalw.c>
+#include <pinguinoserial.c>
