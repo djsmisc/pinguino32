0d34501978ae5c31decb5f57434360ded80610dd
jp.mandon <jp.mandon@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2011-02-23 17:14:36 +0000

    fixed digitalw.c for PIC32-PINGUINO

--- branches/pic32/C32/linux/tmp/temp.c
+++ branches/pic32/C32/linux/tmp/temp.c
@@ -1,51 +1,15 @@
-/*
- * LCD routines for use with pinguino board, based on LiquidCrystal lib from Arduino project.
- * By Marcus Fazzi (Anunakin)
- * Pins, Schematics and more info: 
- * http://pinguino.koocotte.org/index.php/LCD_Example
- * http://anunakin.blogspot.com/2010/08/pinguino-clock.html
- */
 
 
-#define LED 1
-long counter;
-
-void setup(){
-//
-//
-	
-//
-	lcd(0, 2, 3, 4, 5, 6, 0, 0, 0, 0); // RS, E, D4 ~ D8	
-	
-//
-	begin(4, 0);
-	
-	/* Debug */
-	pinmode(LED, OUTPUT);		// LED on pin 13
-	
+void setup()
+{
+pinmode(30,OUTPUT);
 }
- 
+
 void loop()
 {
-//
-	home();
-//
-	lcdPrint("Pinguino32X");	// Output char
-		
-	Delayms(500);
-	while(1){
-	home();
-	lcdPrint("Hello World!");
-	
-	setCursor(0, 1);
-	printNumber(counter, 10);
-	
-	digitalwrite(LED,HIGH);
-	Delayms(250);
-	digitalwrite(LED,LOW);
-	Delayms(250);
-	counter++;
-	}
+digitalwrite(30,HIGH);
+Delayms(500);
+digitalwrite(30,LOW);
+Delayms(500);
 }
-
 
