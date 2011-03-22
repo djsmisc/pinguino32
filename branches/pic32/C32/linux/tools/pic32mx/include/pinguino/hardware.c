85b0bc934748901a5e6471acd890a4c7e6f6535b
anunakin <anunakin@gmail.com>
2011-03-22 17:15:44 +0000

    - updated doc/UBW32 IO mapping .png - added doc/Emperor IO mapping.png - updated hardware.c for use with Emperor Boards - Added two IO pins to Emperor Boards (pins 76/77 PGD/PGC) - revised portmask and pinmasks, at digitalw.c, for all 78 IO pins for Emperor Boards

--- branches/pic32/C32/linux/tools/pic32mx/include/pinguino/hardware.c
+++ branches/pic32/C32/linux/tools/pic32mx/include/pinguino/hardware.c
@@ -1,6 +1,7 @@
 /*	----------------------------------------------------------------------------
 	hardware.c
 	Régis Blanchot
+	* Added support to Emperor Boards - Marcus Fazzi
 	--------------------------------------------------------------------------*/
 
 #ifndef __HARDWARE_C
@@ -11,20 +12,33 @@
 	#include "delay.c"
 	#include "digitalw.c"
 
+#if defined(UBW32)
 	// UBW32 built-in leds
-    #define LED4        4//PORTEbits.RE3	// green led
+    #define LED1		1//PORTEbits.RE2	// white led
+    #define LED2		2//PORTEbits.RE1	// red led
 	#define LED3		3//PORTEbits.RE0	// yellow led
-	#define LED2		2//PORTEbits.RE1	// red led
-	#define LED1		1//PORTEbits.RE2	// white led
+	#define LED4        4//PORTEbits.RE3	// green led	
 
-    #define GREENLED    4//PORTEbits.RE3	// green led
-	#define YELLOWLED	3//PORTEbits.RE0	// yellow led
-	#define REDLED		2//PORTEbits.RE1	// red led
 	#define WHITELED	1//PORTEbits.RE2	// white led
+	#define GREENLED    2//PORTEbits.RE3	// green led	
+	#define YELLOWLED	3//PORTEbits.RE0	// yellow led
+	#define REDLED		4//PORTEbits.RE1	// red led
 
 	// UBW32 built-in buttons
+	#define PROGBUTTON  68	// pin 43 = PORTEbits.RE7 = prog button
     #define USERBUTTON  69	// pin 44 = PORTEbits.RE6 = user button
-    #define PROGBUTTON  68	// pin 43 = PORTEbits.RE7 = prog button
+    
+#elif defined(EMPEROR)
+	// EMPEROR built-in leds
+	#define LED1		1//PORTEbits.RE2	// white led
+	#define LED2		2//PORTEbits.RE3	// red led
+
+    #define BLUELED	    1//PORTEbits.RE2	// blue led
+	#define REDLED		2//PORTEbits.RE3	// red led
+
+	// UBW32 built-in buttons
+    #define USERBUTTON  68	// pin 44 = PORTEbits.RE7 = user button
+#endif
 
 /*	----------------------------------------------------------------------------
 	SwitchDebounce()
@@ -64,20 +78,22 @@ void UBW32LedToggle(u8 led)
 			PORTEbits.RE2 ^= 1;
 			break;
 		case LED2:
-			//LATEbits.LATE1 = 1;
-			TRISEbits.TRISE1 = OUTPUT;
-			PORTEbits.RE1 ^= 1;
+			//LATEbits.LATE3 = 1; // open drain output
+			TRISEbits.TRISE3 = OUTPUT;
+			PORTEbits.RE3 ^= 1;
 			break;
+#ifdef UBW32
 		case LED3:
 			//LATEbits.LATE0 = 1; // open drain output
 			TRISEbits.TRISE0 = OUTPUT;
 			PORTEbits.RE0 ^= 1;
 			break;
         case LED4:
-			//LATEbits.LATE3 = 1; // open drain output
-			TRISEbits.TRISE3 = OUTPUT;
-			PORTEbits.RE3 ^= 1;
+			//LATEbits.LATE1 = 1;
+			TRISEbits.TRISE1 = OUTPUT;
+			PORTEbits.RE1 ^= 1;
 			break;
+#endif
 	}
 }
 
