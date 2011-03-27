d5c54c284f00ffdfe30c656ac65f8bf47ff43844
jp.mandon <jp.mandon@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2011-03-27 20:13:26 +0000

    added support for scratch toolchain

--- branches/pic32/C32/linux/tmp/temp.c
+++ branches/pic32/C32/linux/tmp/temp.c
@@ -1,16 +1,31 @@
 //
 //
 
-void setup(void)
+int led1;						// led1 is an integer ( Pin )
+int led2;						// led2 is an integer ( Pin )
+int tempo;					// tempo is an integer
+
+void setup() 
 {
-pinmode(13,OUTPUT);	// test caractères
+	led1=0;					// led 1 is connected on pin 1
+	led2=1;					// led 2 is connected on pin 2
+	tempo=500;				// tempo for blink is 500 mS
+	pinmode(led1,OUTPUT);		// pin 1 is an output
+	pinmode(led2,OUTPUT);		// pin 7 is an output
 }
 
-void loop(void)
+void loop() 
 {
-digitalwrite(13,HIGH);
-Delayms(500);
-digitalwrite(13,LOW);
-Delayms(500);
-}
+
+	while (1)
+	{
+	digitalwrite(led1,HIGH);		// led 1 high
+	digitalwrite(led2,LOW);		// led 2 low
+	Delayms(tempo);				// wait 0,5 second
+	digitalwrite(led1,LOW);		// led 1 low
+	digitalwrite(led2,HIGH);		// led 2 high
+	Delayms(tempo);				// wait 0,5 second
+	}
+	
+}	
 
