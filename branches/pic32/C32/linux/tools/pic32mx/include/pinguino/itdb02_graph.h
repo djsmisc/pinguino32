b48fbaa5ec3c2ae68f27f0e79af688ac3bc8f6d8
anunakin <anunakin@gmail.com>
2011-04-06 14:22:02 +0000

    - ITDB02 graph revision

--- branches/pic32/C32/linux/tools/pic32mx/include/pinguino/itdb02_graph.h
+++ branches/pic32/C32/linux/tools/pic32mx/include/pinguino/itdb02_graph.h
@@ -77,7 +77,7 @@
 #define FONT_LARGE 2
 
 //Pinguino32X, UBW32, EMPEROR and Minimum boards
-#if defined(UBW32) || defined(EMPEROR)
+#if defined(UBW32_460) || defined(UBW32_795) || defined(EMPEROR460) || defined(EMPEROR795)
 
 #define LCD_DATA_DIR 	TRISD		//Data Direction Register for Data Port
 #define LCD_DATA_BUS 	PORTD		//Data Bus
