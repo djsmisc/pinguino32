ef3ac284938de40a0b1041665a31e6691dc993c7
rblanchot <rblanchot@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2011-04-21 07:11:42 +0000

    Pinguino IDE BetaX 8-bit / sdcc 2.9.0  + gputils 0.13.7 32-bit / g++ 4.5.2 + binutils 2.21 + newlib 1.19 No more support for Microchip C32 Compiler

---
+++ tags/9.5/tmp/mysound.c
@@ -0,0 +1,57 @@
+/*	----------------------------------------------------------------------------
+	Pinguino example to play musical sound with a buzzer or a little loud speaker
+	----------------------------------------------------------------------------
+	author:		Régis Blanchot
+	first release:	08/10/2010
+	last update:	12/10/2010
+	IDE:			Pinguino b9.2
+	----------------------------------------------------------------------------
+	To connect your piezo buzzer or little loud speaker to your pinguino board
+	just take a look at buzzer.jpg
+	--------------------------------------------------------------------------*/
+
+#define LINE_OUT 12			// Buzzer or speaker must be connected
+						// to pin 11 (RC1/CCP2) or 12 (RC2/CCP1) 
+
+// partitions
+
+const u8 gamme[] =	{
+	DO, R1,
+	RE, R1,
+	MI, R1,
+	FA, R1,
+	SOL, R1,
+	LA, R1,
+	SI, R1,
+	DO_, R1,
+	0, 0			};
+
+const u8 jinglebells[] =	{
+	E,R1_2,E,R1_2,E,R1,
+	E,R1_2,E,R1_2,E,R1,
+	E,R1_2,G,R1_2,C,R1_2,D,R1_2,E,R1,
+	F,R1_2,F,R1_2,F,R1_2,F,R1,
+	E,R1_2,E,R1_2,E,R1,
+	D,R1_2,D,R1_2,E,R1_2,D,R1,
+	G,R1,
+	E,R1_2,E,R1_2,E,R1,
+	E,R1_2,E,R1_2,E,R1,
+	E,R1_2,G,R1_2,C,R1_2,D,R1_2,E,R1,
+	F,R1_2,F,R1_2,F,R1_2,F,R1,
+	E,R1_2,E,R1_2,E,R1,
+	G,R1_2,F,R1_2,E,R1_2,D,R1_2,C,R1,
+	0, 0
+					};
+
+void setup()
+{
+	Play(LINE_OUT, gamme,		10);
+	delay(1000);
+}
+
+void loop()
+{
+	Play(LINE_OUT, jinglebells,	10);
+	delay(1000);
+}
+
---
+++ tags/9.5/tmp/temp.c
@@ -0,0 +1,40 @@
+//
+//
+
+char *string = "Hello world!";
+char c = 65;
+int i = 333;
+long l = 115200;
+float f = 3.14159265; // floating point support is not yet implemented in printf
+void setup()
+{
+	serial_begin(9600);
+}
+void loop()
+{
+	serial_printf("\r\n");
+	serial_printf("**************************\r\n");
+	serial_printf("*** Serial Printf Demo ***\r\n");
+	serial_printf("**************************\r\n");
+	serial_printf("\r\n");
+	serial_printf("string = %s\r\n", string);
+	serial_printf("character = \"%c\"\r\n", c);
+	serial_printf("signed char = %d / unsigned char = %u\r\n", -c, -c);
+	serial_printf("signed int = %d / unsigned int = %u\r\n", -i, -i);
+	serial_printf("signed long = %d / unsigned long = %u\r\n", -l, -l);
+	serial_printf("decimal[%d] = hexa[0x%X] = binary[0b%016b] = octal[%o]\r\n", i, i, i, i);
+	serial_printf("float = %f\r\n", f);
+	serial_printf("justif: \"%-10s\"\r\n", "left");
+	serial_printf("justif: \"%10s\"\r\n", "right");
+	serial_printf(" 3: %04d zero padded\r\n", 3);
+	serial_printf(" 3: %-4d left justif.\r\n", 3);
+	serial_printf(" 3: %4d right justif.\r\n", 3);
+	serial_printf("-3: %04d zero padded\r\n", -3);
+	serial_printf("-3: %-4d left justif.\r\n", -3);
+	serial_printf("-3: %4d right justif.\r\n", -3);
+	serial_printf("\r\n");
+	serial_printf("Press Any Key to continue ...\r\n");
+	serial_getkey();
+}
+
+
