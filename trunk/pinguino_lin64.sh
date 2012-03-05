df818dc46bf1f8f2da66bf1d3c7444eba3ff4999
anunakin@gmail.com <anunakin@gmail.com@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2012-03-05 14:13:25 +0000

    - Pinguino Linux 64bits loader updated

--- trunk/pinguino_lin64.sh
+++ trunk/pinguino_lin64.sh
@@ -1,1 +1,7 @@
-LD_LIBRARY_PATH=/usr/lib32:~/dev/Pinguino-svn/pinguino32/trunk/linux/p32/bin:/usr/lib:/usr/lib64 ./pinguino.py
+#!/bin/bash
+
+#Loader for Linux 64 bits
+# 03-03-2012 - First version - Marcus Fazzi <marcus@fazzi.eng.br>
+
+LOCALPATH=$(pwd)
+LD_LIBRARY_PATH=/usr/lib32:${LOCALPATH}/linux/p32/bin:/usr/lib:/usr/lib64 ./pinguino.py
