2ec3827e8b8e265147570d874eef7346d8135a35
hgmvanbeek@gmail.com <hgmvanbeek@gmail.com@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2012-04-06 16:20:58 +0000

    DCF77 altered and tested on Pinguino_Micro; bcd.c made available.

--- branches/x.3/p32/include/pinguino/libraries/dcf77.c
+++ branches/x.3/p32/include/pinguino/libraries/dcf77.c
@@ -28,80 +28,40 @@
 	--------------------------------------------------------------------------*/
 
 #ifndef _DCF77_C_
-#define _DCF77_C_
+ #define _DCF77_C_
 
 #include <typedef.h>			// u8, u16, ...
 #include <digitalw.c>			// pinMode, ...
 #include <millis.c>				// millis
-#ifdef DEBUG					// NB: Turn debugging on or off from the IDE 
-#include <debug.c>
+#ifdef DEBUG					// NB: Turn debugging on or off from the IDE
+ #define UART1DEBUG
+ #include <debug.c>
 #endif
-
-//#include <bcd.c> 
-#define bin2bcd(x)	(x - ( (x/16) *6))
+#include <DCF77.h>
+#include <bcd.c>
+//#define bin2bcd(x)	(x - ((x/16) * 6))
+//#define bcd2bin(bcd) (bcd >> 4) * 10 + bcd % 16;
 
 /*******************************************************************************
  * Number of milliseconds to elapse before we assume a "1",
  * if we receive a falling flank before - its a 0.
 *******************************************************************************/
 
-#define DCF_split_millis 140
+#define DCF_Discr_millis 140
 
 /*******************************************************************************
  * There is no signal in second 59 - detect the beginning of a new minute.
 *******************************************************************************/
 
-#define DCF_sync_millis 1200
-
-/*******************************************************************************
- * DCF time format struct
-*******************************************************************************/
-
-typedef struct
-{
-	unsigned prefix		:21	;
-	unsigned Min		:7	;	//7 Bits für die Minuten
-	unsigned P1			:1	;	//Parity Minuten
-	unsigned Hour		:6	;	//6 Bits für die Stunden
-	unsigned P2			:1	;	//Parity Stunden
-	unsigned Day		:6	;	//6 Bits für den Tag
-	unsigned Weekday	:3	;	//3 Bits für den Wochentag 
-	unsigned Month		:5	;	//3 Bits für den Monat
-	unsigned Year		:8	;	//8 Bits für das Jahr **eine 5 für das Jahr 2005**
-	unsigned P3			:1	;	//Parity von P2
-	u8  pin;
-	u8  previousSignalState;
-	u8  ss, mm, hh, day, mon;
-	u16 year;
-	u16 previousFlankTime;
-	u16 bufferPosition;
-	u32 dcf_rx_buffer;
-} DCF77Struct;
+#define DCF_Sync_millis 1200
 
 /*******************************************************************************
 * Global variables
 *******************************************************************************/
 
-DCF77Struct DCF77;
-
-/*******************************************************************************
-* Prototypes
-*******************************************************************************/
-
-// public
-void DCF77_init(u8);
-u16  DCF77_scanSignal(void);
-u8  DCF77_getSeconds();
-u8  DCF77_getMinutes();
-u8  DCF77_getHours();
-u8  DCF77_getDay();
-u8  DCF77_getMonth();
-u16 DCF77_getYear();
-
-// private
-static void DCF77_appendSignal(u8);
-static void DCF77_finalizeBuffer(void);
-static void DCF77_addSecond();
+u8  SignalPin, PrevSignal, BuffPntr;
+u16 PrevFlank, SyncTimout;
+u32 Buff1, Buff2;
 
 /*******************************************************************************
 * Initialize the DCF77 library.
@@ -110,140 +70,171 @@ static void DCF77_addSecond();
 
 void DCF77_init(u8 dcfPin)
 {
-	DCF77.pin = dcfPin;
-	DCF77.previousSignalState=0;
-	DCF77.previousFlankTime=0;
-	DCF77.bufferPosition=0;
-	DCF77.dcf_rx_buffer=0;
-	DCF77.ss=0;
-	DCF77.mm=0;
-	DCF77.hh=0;
-	DCF77.day=0;
-	DCF77.mon=0;
-	DCF77.year=0;
-	#ifdef DEBUG 
-	debug("Initializing DCF77 lib. using pin #%d\r\n", DCF77Pin);
-	#endif
-	pinmode(DCF77.pin, INPUT);
+	SignalPin  = dcfPin;
+	pinmode(SignalPin, INPUT);
+
+	PrevSignal	= 0;
+	PrevFlank	= 0;
+	BuffPntr	= 0;
+	Buff1 		= 0;
+	Buff2 		= 0;
 }
 
-static void DCF77_appendSignal(u8 signal)
+void Disp_Input_Binary(u32 Bits1, u32 Bits2)
 {
-	#ifdef DEBUG
-	debug("Appending value %d at position %d\r\n", signal, DCF77.bufferPosition);
-	#endif
-	DCF77.dcf_rx_buffer = DCF77.dcf_rx_buffer | ((u32) signal << DCF77.bufferPosition);
-	DCF77.bufferPosition++;
-	if (DCF77.bufferPosition > 59)
-		DCF77_finalizeBuffer();
+  u8 BitC;
+	serial1printf("Bits= %08x %08x \r\n", Bits1, Bits2);
+    for (BitC = 32; BitC > 0; BitC--)
+    {
+		if ((Bits2 & (1 << (BitC-1))) > 0)
+			serial1write('1');
+		else
+			serial1write('0');
+	}
+    for (BitC = 32; BitC > 0; BitC--)
+    {
+		if ((Bits1 & (1 << (BitC-1))) > 0)
+			serial1write('1');
+		else
+			serial1write('0');
+	}
+	serial1println("  ");
 }
 
-static void DCF77_finalizeBuffer(void)
+void DCF77_appendSignal(u8 signal)
 {
-	DCF77Struct *rx_buffer;
-	#ifdef DEBUG
-	debug("Finalizing Buffer\r\n");
-	#endif
-	if (DCF77.bufferPosition == 59)
+//	serial1printf("BuffPntr: %02d  ", BuffPntr);
+	if ((signal > 0) && (BuffPntr < 32))
+		Buff1 = Buff1 | (1 << BuffPntr);
+	if ((signal > 0) && (BuffPntr > 31))
+		Buff2 = Buff2 | (1 << (BuffPntr - 32));
+//	serial1printf("Append Buff: %08x %08x  \r\n", DCF77.Buff1, DCF77.Buff2);
+	BuffPntr++;
+	if (BuffPntr > 59)
 	{
-		rx_buffer = (DCF77Struct *)(u32)&DCF77.dcf_rx_buffer;
-		//convert the received bits from BCD
-		DCF77.mm  = bin2bcd(rx_buffer->Min);
-		DCF77.hh  = bin2bcd(rx_buffer->Hour);
-		DCF77.day = bin2bcd(rx_buffer->Day); 
-		DCF77.mon = bin2bcd(rx_buffer->Month);
-		DCF77.year= bin2bcd(rx_buffer->Year) + 2000;
+		BuffPntr = 0;
+		Buff1 	 = 0;
+		Buff2 	 = 0;
+	}
+}
+
+u8 Parity_Even (u32 Bits)
+{
+  u8 i;
+  u8 p = 0;
+	for (i = 0; i < 32; i++)
+		if ((Bits & (1 << i)) > 0)
+			p = !p;
+	return p;
+}
+
+
+void DCF77_finalizeBuffer(void)
+{
+  u8 Code, Par1, Par2, Par3;
+//	Disp_Input_Binary(Buff1, Buff2);
+
+	if (BuffPntr == 59)
+{
+		Code = (Buff1 >> 21) & 0x7F;
+		Par1 = Parity_Even(Code);
+		if ((Par1 & 1) == ((Buff1 >> 28) & 1))
+		{
+//			serial1printf("Par1= %1x   ", Par1);
+			DCF77.mm = bcd2bin(Code);
+		}
+		else
+		{
+			DCF77.mm++;
+			if (DCF77.mm > 59)
+				DCF77.mm = 0;
+				DCF77.hh++;
+				if (DCF77.hh > 23)
+					DCF77.hh = 0;
+		}
+//		serial1printf("mm= %02x  %d \r\n", Code, DCF77.mm);
+
+		Code = ((Buff1 >> 29) & 0x7) | ((Buff2 & 0x7) << 3);
+		Par2 = Parity_Even (Code);
+		if ((Par2 & 1) == ((Buff2 >> 3) & 1))
+		{
+//			serial1printf("Par2= %1x   ", Par2);
+			DCF77.hh = bcd2bin(Code);
+		}
+//		serial1printf("hh= %02x  %d \r\n", Code, DCF77.hh);
+
+		Code = (Buff2 >> 4) & 0x3FFFFF;
+		Par3 = Parity_Even (Code);
+		if ((Par3 & 1) == ((Buff2 >> 26) & 1))
+		{
+//			serial1printf("Par3= %1x   ", Par3);
+
+			Code = (Buff2 >> 10) & 0x7;
+			DCF77.dw = bcd2bin(Code);
+//			serial1printf("dw= %02x  %02d \r\n", Code, DCF77.dw);
+
+			Code = (Buff2 >> 4) & 0x3F;
+			DCF77.da = bcd2bin(Code);
+//			serial1printf("da= %02x  %02d \r\n", Code, DCF77.da);
+
+			Code = (Buff2 >> 13) & 0x1F;
+			DCF77.mo = bcd2bin(Code);
+//			serial1printf("mo= %02x  %02d \r\n", Code, DCF77.mo);
+
+			Code = (Buff2 >> 18) & 0xFF;
+			DCF77.yr = bcd2bin(Code);
+//			serial1printf("yr= %02x  %d \r\n", Code, DCF77.yr);
+			SyncTimout = 0;
+			DCF77.sync = 1;
+		}
+//		serial1printf("Tim=  %d:%02d \r\n", DCF77.hh, DCF77.mm);
+//		serial1printf("Dat=: %02d-%02d-%02d  %1d\r\n", DCF77.da, DCF77.mo, DCF77.yr, DCF77.dw);
 	}
 	// reset stuff
-	DCF77.ss = 0;
-	DCF77.bufferPosition = 0;
-	DCF77.dcf_rx_buffer=0;
+	DCF77.ss    = 0;
+	BuffPntr    = 0;
+	Buff1 		= 0;
+	Buff2 		= 0;
 }
 
 /*******************************************************************************
- * Should be called every 15 ms in order to sample the DCF signal.
+ * Should be called every 20 ms in order to sample the DCF signal.
  * Returns the current state of the signal: 1 for HIGH, 0 for LOW.
  ******************************************************************************/
- 
-u16 DCF77_scanSignal(void)
-{
-	u16 difference;
-	u16 thisFlankTime;
-	u8 DCFsignal = digitalread(DCF77.pin);
 
-	if (DCFsignal != DCF77.previousSignalState)
+u8 DCF77_scanSignal(void)
+{
+  u16 LengthPulse;
+  u16 CurrFlank;
+  u8  CurrSignal ;
+    CurrSignal = digitalread(SignalPin);
+	if (CurrSignal != PrevSignal)
 	{
-		if (DCFsignal == 1)
+		if (CurrSignal == 1)
 		{
-			/* We detected a raising flank, increase per one second! */
-			DCF77_addSecond();
-			#ifdef DEBUG
-			debug("Time: %d:%d:%d\r\n", hh, mm, ss);
-			debug("Date: %d.%d.%d\r\n", day, mon, year);
-			#endif
-			thisFlankTime=millis();
-			if (thisFlankTime - DCF77.previousFlankTime > DCF_sync_millis)
-			{
-				#ifdef DEBUG
-				debug("####\r\n");
-				debug("#### Begin of new Minute!!!\r\n");
-				debug("####\r\n");
-				#endif
+			/* We detected a raising flank */
+			DCF77.ss++;
+			if (DCF77.ss > 59)
+				DCF77.ss = 0;
+			CurrFlank = millis();
+			if (CurrFlank - PrevFlank > DCF_Sync_millis)
 				DCF77_finalizeBuffer();
-			}
-			DCF77.previousFlankTime = thisFlankTime;
-			#ifdef DEBUG
-			debug("%d : DCF77 Signal detected,\r\n", DCF77.previousFlankTime);
-			#endif
+			PrevFlank = CurrFlank;
 		}
 		else
 		{
 			/* or a falling flank */
-			difference = millis() - DCF77.previousFlankTime;
-			#ifdef DEBUG
-			debug("duration: %d\r\n", difference);
-			#endif
-			if (difference < DCF_split_millis)
+			LengthPulse = millis() - PrevFlank;
+			if (LengthPulse < DCF_Discr_millis)
 				DCF77_appendSignal(0);
 			else
 				DCF77_appendSignal(1);
 		}
-		DCF77.previousSignalState = DCFsignal;
+		PrevSignal = CurrSignal;
 	}
-	return DCFsignal;
+	if (SyncTimout++ > 3000)
+		DCF77.sync = 0;
+	return CurrSignal;
 }
-
-/******************************************************************************
- * Add one second to the hh:mm:ss variables.
- ******************************************************************************/
-
-static void DCF77_addSecond()
-{
-	DCF77.ss++;
-	if (DCF77.ss==60)
-	{
-		DCF77.ss=0;
-		DCF77.mm++;
-		if (DCF77.mm==60)
-		{
-			DCF77.mm=0;
-			DCF77.hh++;
-			if (DCF77.hh==24) 
-				DCF77.hh=0;
-		}
-	}
-}
-
-/******************************************************************************
- * Functions to get Time and Date
- ******************************************************************************/
-
-u8  DCF77_getSeconds()	{ return DCF77.ss;   }
-u8  DCF77_getMinutes()	{ return DCF77.mm;   }
-u8  DCF77_getHours()	{ return DCF77.hh;   }
-u8  DCF77_getDay()		{ return DCF77.day;  }
-u8  DCF77_getMonth()	{ return DCF77.mon;  }
-u16 DCF77_getYear()		{ return DCF77.year; }
-
 
 #endif
