353e8d0d5c13801cff5026d9ee67b303640270cb
bjoernp72@gmail.com <bjoernp72@gmail.com@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2012-04-10 19:57:54 +0000

    Small changes....

--- branches/x.3/pinguino_start.sh
+++ branches/x.3/pinguino_start.sh
@@ -7,17 +7,18 @@ LOCALPATH="$(dirname -- $(readlink -f -- "${0}") )"
 cd $LOCALPATH
 
 # Set LD_LIBRARY_PATH to have our libs take precedence over what may be installed
-case $(arch) in
+ARCH=$(arch)
+case $ARCH in
     *64 )
     # catch "ia64" (Itanium) and  "x86_64" 
          LD_LIBRARY_PATH=/usr/lib32:${LOCALPATH}/linux/p32/bin:/usr/lib:/usr/lib64     
 	 ;;
     * )
-    # i386 and the rest goes here.
+    # i386 and the rest land here.
         LD_LIBRARY_PATH=/usr/lib32:${LOCALPATH}/linux/p32/bin:/usr/lib
 	;;
 esac
 
 export LD_LIBRARY_PATH
-./pinguino.py
+python ./pinguino.py
 
