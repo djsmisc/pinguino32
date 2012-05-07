22e07f37654b6884114eb222247729e013ff9d92
yeison.eng <yeison.eng@gmail.com>
2012-05-07 03:09:48 +0000

    Add option cancel on "Close".

--- branches/x.3/wxgui/editor/lateral_tool_area/dock_documents.py
+++ branches/x.3/wxgui/editor/lateral_tool_area/dock_documents.py
@@ -7,7 +7,7 @@
     author:		Yeison Cardona
     contact:		yeison.eng@gmail.com 
     first release:	31/March/2012
-    last release:	31/March/2012
+    last release:	06/May/2012
     
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
