59043f4ec2c8c562ec33283fae38a8cb4e7d1889
yeison.eng@gmail.com <yeison.eng@gmail.com@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2012-11-25 19:01:58 +0000

    Fix board selector and aggregate first stage of new board selector.

--- branches/x.4/wxgui/frames/__init__.py
+++ branches/x.4/wxgui/frames/__init__.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from framesX4 import framePinguinoX, frameKeyWords, \
-                     framePreferences, panelLateral, \
-                     menubarPinguino, frameAutoCompleter, \
-                     panelOutput, frameStdout, \
-                     frameUpgrade, FrameSelectDevice
+from framesX4 import *
