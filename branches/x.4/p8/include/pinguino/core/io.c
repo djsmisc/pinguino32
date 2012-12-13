46e9ec70e966f4359cb0738a73f37a7ed8b45ef0
rblanchot <rblanchot@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2012-12-13 16:40:42 +0000

    P8: moved Osc. wait loop in io.c

--- branches/x.4/p8/include/pinguino/core/io.c
+++ branches/x.4/p8/include/pinguino/core/io.c
@@ -36,6 +36,25 @@
 
 void IOsetSpecial()
 {
+    #if defined(PIC18F26J50) || defined(PIC18F46J50)
+
+        // Enable the PLL and wait 2+ms until the PLL locks
+        u16 pll_startup_counter = 600;
+        OSCTUNEbits.PLLEN = 1;
+        while (pll_startup_counter--);
+        // if user switch to INTOSC _31KHZ_ then
+        // select INTOSC/256 as a 31.25 KHz clock source
+        OSCTUNEbits.INTSRC = 1;
+
+    #elif defined(PIC18F25K50) || defined(PIC18F45K50)
+
+        OSCCON = 0x70;              // 0b01110000 : 111 = HFINTOSC (16 MHz)
+        while (!OSCCONbits.HFIOFS); // wait HFINTOSC frequency is stable (HFIOFS=1) 
+
+    #else
+        nop();
+        
+    #endif
 }
 
 // All Analog Pins as Digital IOs
@@ -58,6 +77,10 @@ void IOsetDigital()
             ANSELD = 0;			    // all I/O to Digital mode
             ANSELE = 0;			    // all I/O to Digital mode
         #endif
+
+    #else
+        nop();
+        
     #endif
 }
 
@@ -82,6 +105,7 @@ void IOsetDigital()
  * 16   RA3         -
  * 17   RA5         RP2
  **/
+ 
 // Peripheral Remappage
 // NB1 : the Configuration bit IOL1WAY is set to OFF in the bootloader
 // NB2 : pins must be explicitly reconfigured as digital I/O when used with a PPS
@@ -123,6 +147,9 @@ void IOsetRemap()
         SystemUnlock();
         PPSCONbits.IOLOCK = 1;			// Turn on PPS Write Protect
         SystemLock();
+
+    #else
+        nop();
 
     #endif /* defined(PIC18F26J50) || defined(PIC18F46J50) */
 }
