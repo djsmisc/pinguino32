14e6a4c710c681a80e88e105929f665e792bfcfe
rblanchot <rblanchot@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2013-01-15 21:33:24 +0000

    P8 : new p8 tree

---
+++ branches/x.4/p8/pinguino/libraries/sd/diskio.h
@@ -0,0 +1,41 @@
+/*-----------------------------------------------------------------------
+/  PFF - Low level disk interface modlue include file    (C)ChaN, 2009
+/-----------------------------------------------------------------------*/
+
+#ifndef _DISKIO
+#define _DISKIO
+
+/* Status of Disk Functions */
+typedef u8	DSTATUS;
+
+
+/* Results of Disk Functions */
+typedef enum {
+	RES_OK = 0,		/* 0: Function succeeded */
+	RES_ERROR,		/* 1: Disk error */
+	RES_NOTRDY,		/* 2: Not ready */
+	RES_PARERR		/* 3: Invalid parameter */
+} DRESULT;
+
+
+/*---------------------------------------*/
+/* Prototypes for disk control functions */
+
+DSTATUS disk_initialize (void);
+DRESULT disk_readp (u8*, u32, u16, u16);
+DRESULT disk_writep (const u8*, u32);
+u16 decreasetim(u16);
+
+#define STA_NOINIT		0x01	/* Drive not initialized */
+#define STA_NODISK		0x02	/* No medium in the drive */
+
+/* Card type flags (CardType) */
+#define CT_MMC				0x01	/* MMC ver 3 */
+#define CT_SD1				0x02	/* SD ver 1 */
+#define CT_SD2				0x04	/* SD ver 2 */
+#define CT_SDC				(CT_SD1|CT_SD2)	/* SD */
+#define CT_BLOCK			0x08	/* Block addressing */
+
+u16 Tim1=0, Tim2=0;		/* 1000Hz decrement timer */
+
+#endif
