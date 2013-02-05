8b61f3b369736a2e0a011956ca1370d5deffcb79
rblanchot <rblanchot@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2013-02-05 19:47:06 +0000

    P8 : updated main.c

--- branches/x.4/source/main.c
+++ branches/x.4/source/main.c
@@ -4,13 +4,14 @@
     PURPOSE:        application main function
     PROGRAMER:      Jean-pierre Mandon - Régis Blanchot
     FIRST RELEASE:  19 Sep 2008
-    LAST RELEASE:   14 Dec. 2012
+    LAST RELEASE:   05 Jan 2013
     ----------------------------------------------------------------------------
     CHANGELOG :
     Originally based on a file by (c) 2006 Pierre Gaufillet <pierre.gaufillet@magic.fr>
     19 Sep 2008 - Jean-pierre Mandon - adapted to Pinguino  
     21 Apr 2012 - Régis Blanchot - added bootloader v4.x support
     20 Jun 2012 - Régis Blanchot - added io.c support (remapping)
+    05 Fev 2013 - Régis Blanchot - added interrupt init
     ----------------------------------------------------------------------------
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -28,9 +29,10 @@
     -------------------------------------------------------------------------*/
 
 #include <pic18fregs.h>
+#include <typedef.h>
 #include <const.h>
 #include <macro.h>
-#include <typedef.h>
+
 
 #ifdef boot2
     #include <common_types.h>
@@ -38,16 +40,15 @@
 #endif
 
 #ifdef boot4
-    // runtime start code with variable initialisation
+    // runtime start code with variables initialisation
     #include "crt0iPinguino.c"
+    /*
+    #ifdef __USB__
+        #include <usb.h>
+        #include <usb.c>
+    #endif
+    */
 #endif
-
-/*
-#ifdef __USB__
-    #include <usb.h>
-    #include <usb.c>
-#endif
-*/
 
 // only for compatibility with application_iface.o
 #ifdef boot2
@@ -63,7 +64,6 @@
 // files that need define.h to be included first
 #include <pin.h>
 #include <io.c>
-
 // user's .pde file translated to C
 #include "user.c"
 
@@ -75,91 +75,86 @@ void main(void)
 void pinguino_main(void)
 #endif
 {
-    SystemWaitForStableOsc();
-    SystemDisablePeripheralInterrupt();
+    // Perform a loop for some processors until their frequency is stable
+
+    #if defined(__18f26j50) || defined(__18f46j50)
+
+        // Enable the PLL and wait 2+ms until the PLL locks
+        u16 pll_startup_counter = 600;
+        OSCTUNEbits.PLLEN = 1;
+        while (pll_startup_counter--);
+
+    #elif defined(CHRP3)
     
-    IOsetSpecial();
+        // wait HFINTOSC frequency is stable (HFIOFS=1) 
+        while (!OSCCONbits.HFIOFS);
+
+    #endif
+
+    // Interrupt init
+
+    RCON   = 0x80;                // Enable priority levels on interrupts
+/*
+    INTCON = 0;     INTCON2 = 0;    INTCON3 = 0;
+    PIR1   = 0;     PIR2    = 0;  // Flag
+    PIE1   = 0;     PIE2    = 0;  // Enable
+    IPR1   = 1;     IPR2    = 1;  // Priority
+    #if defined(__18f26j50) || defined(__18f46j50)
+    PIR3    = 0;    PIE3    = 0;    IPR3    = 1;
+    #endif
+*/
+    // I/O init 
+
     IOsetDigital();
     IOsetRemap();
     
-    #ifdef ON_EVENT             // Enable General/Peripheral interrupts
-    int_init();                 // Disable all individual interrupts
+    #ifdef ON_EVENT
+    IntInit();
     #endif
 
     #ifdef __USB__
-		#if defined(__18f25k50) || defined(__18f45k50)
-			PIE3bits.USBIE = 1;
-        #else
-			PIE2bits.USBIE = 1;
-        #endif
-        INTCONbits.PEIE = 1;
-        INTCONbits.GIE  = 1;
+    usb_init();
     #endif
 
-    //setup();
+    #ifdef __USBCDC
+    CDC_init();
+    #endif    
 
-    //#ifdef ON_EVENT
-    //int_start();                // Enable all defined timers interrupts
-    //#endif
+    #ifdef __USBBULK
+    bulk_init();
+    #endif
 
     #ifdef ANALOG
     analog_init();
     #endif
 
-    #ifdef __MILLIS__          // Use Timer 0
+    #ifdef __MILLIS__           // Use Timer 0
     millis_init();
     #endif
 
-    #ifdef SERVOSLIBRARY
+    #ifdef SERVOSLIBRARY        // Use Timer 1
     servos_init();
-    #endif
-
-    #ifdef __USBCDC
-        CDC_init();
-		#if defined(__18f25k50) || defined(__18f45k50)
-			PIE3bits.USBIE = 1;
-        #else
-			PIE2bits.USBIE = 1;
-        #endif
-        INTCONbits.PEIE = 1;
-        INTCONbits.GIE  = 1;
-    #endif    
-
-    #ifdef __USBBULK
-        bulk_init();
-		#if defined(__18f25K50) || defined(__18f45K50)
-			PIE3bits.USBIE = 1;
-        #else
-			PIE2bits.USBIE = 1;
-        #endif
-        INTCONbits.PEIE = 1;
-        INTCONbits.GIE  = 1;
     #endif
 
     #ifdef __PS2KEYB__
     keyboard_init()
     #endif
 
-    #if defined(__SERIAL__) || defined(SERVOSLIBRARY)
-    INTCONbits.PEIE = 1;
-    INTCONbits.GIE  = 1;
-    #endif 
-
-/*  RB : millis.c/millis_init() did already the job
-    #ifdef MILLIS
-    INTCONbits.TMR0IE= 1;
-    INTCONbits.GIE  = 1;
-    #endif 
-*/
-
+////////////////////////////////////////////////////////////////////////
     setup();
+////////////////////////////////////////////////////////////////////////
     
-    #ifdef ON_EVENT
-    int_start();                // Enable all defined timers interrupts
+    #if defined(TMR0INT) || defined(TMR1INT) || \
+        defined(TMR2INT) || defined(TMR3INT) || \
+        defined(TMR4INT)
+    IntTimerStart();                // Enable all defined timers interrupts
     #endif
 
-    while (1)
+    while (1) {
+////////////////////////////////////////////////////////////////////////
         loop();
+////////////////////////////////////////////////////////////////////////
+    }
 }
 
 /*  ----------------------------------------------------------------------------
@@ -168,94 +163,60 @@ void pinguino_main(void)
 
 #ifdef boot2
 
-    #pragma code high_priority_isr 0x2020
-    void high_priority_isr(void) __interrupt 1
+#pragma code high_priority_isr 0x2020
+void high_priority_isr(void) __interrupt 1
 
 #else
 
-    // ENTRY + 0x08
-    void high_priority_isr(void) __interrupt 1
+// boot4 : ENTRY + 0x08
+// noboot: 0x08
+void high_priority_isr(void) __interrupt 1 //__naked
 
 #endif
 {
-#if defined(__USBCDC) || defined(__USBBULK)
-    #if defined(__18f25k50) || defined(__18f45k50)
-    if(PIR3bits.USBIF)
-    #else
-    if(PIR2bits.USBIF)
+    #ifdef __USBCDC 
+    CDC_interrupt();
     #endif
-    {
-        ProcessUSBTransactions();
-        UIRbits.SOFIF = 0;
-        UIRbits.URSTIF = 0;
-        #if defined(__18f25k50) || defined(__18f45k50)
-        PIR3bits.USBIF = 0;
-        #else
-        PIR2bits.USBIF = 0;
-        #endif
-        UEIR = 0;
-    }
-#endif
-
-#ifdef __USB__
-    #if defined(__18f25k50) || defined(__18f45k50)
-    if(PIR3bits.USBIF)
-    #else
-    if(PIR2bits.USBIF)
+    
+    #if defined(__USBBULK)
+    bulk_interrupt();
     #endif
-    {
-        dispatch_usb_event();
-        UIRbits.SOFIF = 0;
-        UIRbits.URSTIF = 0;
-        PIR2bits.USBIF = 0;
-        UEIR = 0;
-    }
-#endif
 
-#ifdef __SERIAL__
-    #if defined(__18f1220)  || defined(__18f1320)   || \
-        defined(__18f14k22) || defined(__18lf14k22) || \
-        defined(__18f2550)  || defined(__18f4550)   || \
-        defined(__18f25k50) || defined(__18f45k50)  || \
-        defined(__18f2455)  || defined(__18f4455)
-        if (PIR1bits.RCIF) 
-    #elif defined(__18f26j50) || defined(__18f46j50)
-        if (PIR1bits.RC1IF) 
-    #else
-        #error "Processor Not Yet Supported. Please, Take Contact with Developpers."
+    #ifdef __USB__
+    usb_interrupt();
     #endif
-        serial_interrupt();
-#endif
 
-#ifdef __MILLIS__
-    if (INTCONbits.TMR0IF)
-    {
-        INTCONbits.TMR0IF=0;
-        TMR0H=0xE8;
-        TMR0L=0x50;
-        _millis++;
-    }    
-#endif
+    #ifdef __SERIAL__
+    serial_interrupt();
+    #endif
 
-//#ifdef __I2C__
-//    I2C_interrupt();
-//#endif
+    #ifdef __MILLIS__
+    millis_interrupt();
+    #endif
 
-#ifdef SERVOSLIBRARY
+    #ifdef I2CINT
+    I2C_interrupt();
+    #endif
+
+    #ifdef SERVOSLIBRARY
     servos_interrupt();
-#endif
+    #endif
 
-#ifdef INT0INT
+    #ifdef INT0INT
     userhighinterrupt();
-#endif
+    #endif
 
-#ifdef __PS2KEYB__
-    keyboard_isr();
-#endif
+    #ifdef __PS2KEYB__
+    keyboard_interrupt();
+    #endif
 
-#ifdef __DCF77__
+    #ifdef __DCF77__
     dcf77_interrupt();
-#endif
+    #endif
+
+    #ifdef RTCCALARMINTENABLE
+    rtcc_interrupt();
+    #endif
 }
 
 /*  ----------------------------------------------------------------------------
@@ -264,22 +225,23 @@ void pinguino_main(void)
 
 #ifdef boot2
 
-    #pragma code low_priority_isr 0x4000
-    void low_priority_isr(void) __interrupt 2
+#pragma code low_priority_isr 0x4000
+void low_priority_isr(void) __interrupt 2
 
 #else
 
-    // ENTRY + 0x18
-    void low_priority_isr(void) __interrupt 2
+// boot4 : ENTRY + 0x18
+// noboot: 0x18
+void low_priority_isr(void) __interrupt 2 //__naked
 
 #endif
 
 {
-#ifdef USERINT
+    #ifdef USERINT
     userinterrupt();
-#endif
+    #endif
 
-#ifdef ON_EVENT
+    #ifdef ON_EVENT
     userlowinterrupt();
-#endif
+    #endif
 }
