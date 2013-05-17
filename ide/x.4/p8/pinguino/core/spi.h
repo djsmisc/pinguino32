d3388fd271cc07bd2d0f086fb17c8c618378b17f
rblanchot <rblanchot@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2013-05-17 12:38:05 +0000

    P8: fixed bug in spi library

--- ide/x.4/p8/pinguino/core/spi.h
+++ ide/x.4/p8/pinguino/core/spi.h
@@ -12,10 +12,10 @@
 #define __SPI_H__
 
 // pins
-#define SDIPIN		TRISBbits.TRISB0
-#define SDOPIN		TRISCbits.TRISC7
-#define SCKPIN		TRISBbits.TRISB1
-#define SSPIN		TRISAbits.TRISA5
+#define SDIPIN		        TRISBbits.TRISB0
+#define SDOPIN		        TRISCbits.TRISC7
+#define SCKPIN		        TRISBbits.TRISB1
+#define SSPIN		        TRISAbits.TRISA5
 
 /*	----------------------------------------------------------------------------
 	---------- *** MODES ***
@@ -34,18 +34,18 @@
 #define SPI_MASTER_FOSC_16	0x01
 #define SPI_MASTER_FOSC_64	0x02
 #define SPI_MASTER_FOSC_T2	0x03
-#define SPI_SLAVE_SS			0x04
-#define SPI_SLAVE				0x05
+#define SPI_SLAVE_SS		0x04
+#define SPI_SLAVE			0x05
 
 #define SPI_CLOCK_DIV4		0x00
 #define SPI_CLOCK_DIV16		0x01
 #define SPI_CLOCK_DIV64		0x02
 #define SPI_CLOCK_TIMER2	0x03
 
-#define SPI_MODE0				0x00
-#define SPI_MODE1				0x04
-#define SPI_MODE2				0x08
-#define SPI_MODE3				0x0C
+#define SPI_MODE0			0x00
+#define SPI_MODE1			0x04
+#define SPI_MODE2			0x08
+#define SPI_MODE3			0x0C
 
 
 #define SPI_FALLING_EDGE	0x00  // negated
