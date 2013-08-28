99a800d2a930e5500abcb87b90868ad436d0a298
rblanchot <rblanchot@b7319616-6d90-1ae0-72d7-295f6d5ed4f0>
2013-08-28 16:48:12 +0000

    P8 : added Pinguino 47J53-A IO mapping support

--- ide/x.4/p8/pinguino/core/typedef.h
+++ ide/x.4/p8/pinguino/core/typedef.h
@@ -22,12 +22,12 @@
 	typedef signed char			s8;
 	typedef signed int			s16;
 	typedef signed long			s32;
-	//typedef signed long long 	s64;
+	//typedef signed long long 	s64;        // SDCC doesn't support 64-bit type
 
 	typedef unsigned char		u8;
 	typedef unsigned int		u16;
 	typedef unsigned long		u32;
-	//typedef unsigned long long 	u64;
+	//typedef unsigned long long 	u64;    // SDCC doesn't support 64-bit type
 	
 /*	----------------------------------------------------------------------------
 	avr-gcc types
